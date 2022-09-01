37fffa5fbc397618d08447e4c5f1aa373ed85f9f
vednt18 <41503108+vednt18@users.noreply.github.com>
2022-09-01 19:16:46 +0530

    Day1 commit

---
+++ Day1/src/Solution1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Valid Username Regular Expression
+ */
+
+class UsernameValidator {
+    public static final String regularExpression = "[a-zA-Z][\\w]{7,29}";
+}
+
+public class Solution1 {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
